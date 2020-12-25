c369a5c885e3ff4a6410587b75a56d2bee9e33b1
Aderbal Filho <aderbalcrato@yahoo.com.br>
2020-12-25 18:41:01 -0300

    fix refresh behavior on github pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import LasersFeelingsSheet from "@pages/LasersFeelingsSheet";
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route exact path="/rpg-facilities/">
+      {/* <Route exact path="/rpg-facilities/">
         <Redirect to="/rpg-facilities/lasers-feelings" />
       </Route>
-      <Route path="/rpg-facilities/lasers-feelings" component={LasersFeelingsSheet} />
+      <Route path="/rpg-facilities/lasers-feelings" component={LasersFeelingsSheet} /> */}
+      <Route exact path="/rpg-facilities/" component={LasersFeelingsSheet} />
     </Switch>
   </Router>
 );
